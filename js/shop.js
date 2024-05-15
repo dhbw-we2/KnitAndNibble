820437518a4982eb99c900b19b0b2ff901447301
Jan Merz <merz.jan@gmx.de>
2024-05-15 15:21:53 +0200

    added code for quantity button on cart page (not functional)

--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,6 @@
 function zumWarenkorb(item) {
     let warenkorb = JSON.parse(localStorage.getItem('Warenkorb'))
-    if (warenkorb == null){
+    if (warenkorb == null) {
         warenkorb = []
     }
 
@@ -8,3 +8,20 @@ function zumWarenkorb(item) {
     warenkorb.push(item)
     localStorage.setItem("Warenkorb", JSON.stringify(warenkorb))
 }
+
+// Product Quantity
+    function quantityButton(btn) {
+        console.log("t")
+        var button = $(btn);
+        var oldValue = button.parent().parent().find('input').val();
+        if (button.hasClass('btn-plus')) {
+        var newVal = parseFloat(oldValue) + 1;
+    } else {
+        if (oldValue > 0) {
+        var newVal = parseFloat(oldValue) - 1;
+    } else {
+        newVal = 0;
+    }
+    }
+        button.parent().parent().find('input').val(newVal);
+    }
