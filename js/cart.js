3dad8098630cb1206bea71d9f9fba6cbd05670aa
Jan Merz <merz.jan@gmx.de>
2024-05-18 18:50:26 +0200

    total calculation in checkout page is fully functional

--- js/cart.js
+++ js/cart.js
@@ -20,6 +20,29 @@ function update_quantity(name, amount_text, textField)  {
     } else {
         textField.value = amount
         warenkorb[index].amount = amount
+        item_total(warenkorb[index], textField.parentElement.parentElement.nextElementSibling.firstElementChild) //There definitely is a better way to navigate through these elements
     }
     localStorage.setItem("Warenkorb", JSON.stringify(warenkorb))
+    update_total()
+}
+
+function item_total(element, textField) {
+    textField.textContent = parseFloat(element.price * element.amount).toFixed(2) + '€' //parseFloat is necessary because of rounding errors
+}
+
+function update_total() {
+    let warenkorb = JSON.parse(localStorage.getItem('Warenkorb'))
+    let total = 0
+    let shipping_cost = 3
+    let free_shipping = 50
+
+    for(let i in warenkorb) {
+        total += warenkorb[i].price * warenkorb[i].amount
+    }
+    total = parseFloat(total).toFixed(2)
+    document.getElementById("subtotal").innerHTML = total
+    if(total <= free_shipping) {
+        total = (parseFloat(total) + shipping_cost).toFixed(2)
+    }
+    document.getElementById("total").innerHTML = total
 }
--- js/daten.js
+++ js/daten.js
@@ -3,21 +3,21 @@ SHOP_ITEM = [
     name: "Self knitted Shoes",
     description: "Hmm Knitty Knitty, some warm shoeys",
     image: "shoes_example_image.jpg",
-    preis: "1€",
+    price: "1",
     category: "wearable"
   },
   {
     name: "Hat",
     description: "A warm comfy hat, awesome for children",
     image: "hat_example_image.jpg",
-    preis: "2",
+    price: "2",
     category: "wearable"
   },
   {
     name: "Soggies",
     description: "Woah, such warm comfy socks, we still need a 2nd line",
     image: "socks_example_image.jpg",
-    preis: "3",
+    price: "2.99",
     category: "wearable"
   }
 ]
