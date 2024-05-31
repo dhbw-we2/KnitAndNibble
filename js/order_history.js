8ad3c345063e193c0337ca359ce4135309a4c822
Jan Merz <merz.jan@gmx.de>
2024-05-31 11:01:00 +0200

    fixed dropdown in order_history and order_confirmation, fixed order_history.js bug, when there are no recent orders

--- js/order_history.js
+++ js/order_history.js
@@ -5,8 +5,13 @@ function show_all_orders()  {
 
 function show_recent_orders()   {
     let recent_index = JSON.parse(sessionStorage.getItem("index_recent_orders"))
-    let order_history = JSON.parse(localStorage.getItem("Order_history"))
-    order_history.orders = order_history.orders.slice(recent_index)
+    if(recent_index != null)    {
+        let order_history = JSON.parse(localStorage.getItem("Order_history"))
+        order_history.orders = order_history.orders.slice(recent_index)
 
-    render(order_history, '#order-list')
+        render(order_history, '#order-list')
+    } else {
+        render(null, '#order-list')
+    }
+
 }
